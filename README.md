3676045690f14ac09d718a4a8d4eac459bfd6583
Vatt Sopheak <vattcarter@gmail.com>
2024-03-29 15:53:54 +0700

    Filtering app

---
+++ README.md
@@ -0,0 +1,11 @@
+A modern approach for product filtering made to demonstrate web dev fundamentals in action :3
+
+### Upstash config
+> create index: product-filter
+> select region: us-east-1
+> select dimensions: 3
+> select distance metrics: EUCLIDEAN
+
+### App config
+> npm i
+> npm run seed
---
+++ src/components/Products/Product.tsx
@@ -0,0 +1,30 @@
+import { Product as TProduct } from '@/db'
+import Image from 'next/image'
+
+const Product = ({ product }: { product: TProduct }) => {
+  return (
+    <div className='group relative'>
+      <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80'>
+        <Image
+          width={100}
+          height={100}
+          src={product.imageId}
+          alt='product image'
+          className='h-full w-full object-cover object-center'
+        />
+      </div>
+      <div className='mt-4 flex justify-between'>
+        <div>
+          <h3 className='text-sm text-gray-700'>{product.name}</h3>
+          <p className='mt-1 text-sm text-gray-500'>
+            Size {product.size.toUpperCase()}, {product.color}
+          </p>
+        </div>
+
+        <p className='text-sm font-medium text-gray-900'>{product.price}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Product
